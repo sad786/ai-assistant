59bf7e1b441aec2b2ed0ff656e3ba069e8679fdf
Saddam Ahmed <sa522408@gmail.com>
2025-03-27 11:46:41 +0530

    Update urls.py
    added homepage url

--- ai_assistant/urls.py
+++ ai_assistant/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from chat.views import redirect_to_chat
+
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('', include('chat.urls')),
+    path('chat/', include('chat.urls')),
+    path('', redirect_to_chat),  # Use the custom view for redirection
 ]
